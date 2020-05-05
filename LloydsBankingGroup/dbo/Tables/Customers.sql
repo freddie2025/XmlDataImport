5c2bdc52021afb38351739fe998c4d417a67350a
Fred James <fredjsam@live.co.uk>
2020-05-05 12:27:00 +0100

    Basic XML Import using an XSD

---
+++ LloydsBankingGroup/dbo/Tables/Customers.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Customers]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[CompanyName] NVARCHAR (255) NULL,
+    [ContactName] NVARCHAR (255) NULL,
+    [ContactTitle] NVARCHAR (255) NULL,
+    [Phone] NVARCHAR (255) NULL,
+    [Fax] NVARCHAR (255) NULL,
+    [FullAddress] NVARCHAR (255) NULL
+)
